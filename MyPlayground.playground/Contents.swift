e3e62594825ff28b685575faac5ed6163fcdb8ac
Darci Rodrigues <darci.rodrigues@fourtime.com>
2018-04-03 17:22:22 -0300

    projetos

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,14 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+print(str)
+
+if (2 % 2) == 0{
+    print("par")
+}
+else{
+    print("impar")
+}
